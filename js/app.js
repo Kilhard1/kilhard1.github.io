95a4a2d55bd8f642a2aa71d32596176fc1529baa
Nikolai <n.repin@redsign.ru>
2018-11-16 22:52:35 +0300

    fix checkbox

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 var app = new Vue({
   el: "#app",
   data: {
-		newItem: '',
 		items: []
   },
 	methods: {
 		addItem: function() {
-console.log("addItem");
             var n = this.items.length;
 			var item = {
                 time: "11:00",
-                name: "Беги, Форест, беги " + n
+				name: "Беги, Форест, беги " + n,
+				check: false
             }
 			this.items.push(item);
 		},
 		removeItem: function(index) {
+console.log(this.items[index].check);			
             this.items.splice(index,1);
 		},
 		clearList: function() {
