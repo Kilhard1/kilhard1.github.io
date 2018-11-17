c716f986f7f41768e80b32591f3f3cea7cc4ffa6
Nikolai <n.repin@redsign.ru>
2018-11-17 22:00:11 +0300

    create modal and sorting function

--- js/app.js
+++ js/app.js
@@ -1,25 +1,51 @@
+
+var sortByTime = function(d1, d2){
+	if(d1.time < d2.time) {
+		return -1;
+	} else {
+		return 1;
+	}
+};
+
 var app = new Vue({
   el: "#app",
   data: {
-		items: []
+	    modal: false,
+		time: '',
+		name: '',
+		items: [],
+		times: [ '10:00', '10:15', '10:30', '10:45', '11:00', '11:15', '11:30', '11:45', '12:00', '12:15', '12:30', '12:45', '13:00', '13:15', '13:30' ]
   },
 	methods: {
-		addItem: function() {
-            var n = this.items.length;
+		addItemForm: function() {
+			if( this.name == '' ) {
+				alert("Нужно заполнить все поля");
+				return
+			};
+			if( this.time == '' ) {
+				alert("Нужно заполнить все поля");
+				return
+			};
 			var item = {
-                time: "11:00",
-				name: "Беги, Форест, беги " + n,
+                time: this.time,
+				name: this.name,
 				check: false
             }
 			this.items.push(item);
+			this.items.sort(sortByTime);
+			this.time = '';
+			this.name = '';
 		},
 		removeItem: function(index) {
-console.log(this.items[index].check);			
             this.items.splice(index,1);
 		},
-		clearList: function() {
-			this.items = [];
-		}
+		modalOn: function() {
+			this.modal = true;
+		},
+		modalOff: function() {
+			this.modal = false;
+		},
+
 	}
 });
 
